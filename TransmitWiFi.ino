3216d37f385b89dbac98bb1e70006675a5643738
magamartella <giantsquids@gmail.com>
2021-06-04 15:43:37 +0200

    Delete TransmitWiFi.ino

--- TransmitWiFi.ino
+++
@@ -1,7 +0,0 @@
-void TransmitWiFi(){
-        pm10 ->    save(PM10);
-        pm25 ->    save(PM25);
-        temp ->    save(Temp);
-        humi ->    save(Humi);
-        VBat ->    save(vBat);
-}
